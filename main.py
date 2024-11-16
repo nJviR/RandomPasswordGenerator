1e51fcb70b07a10b534ef27ca512224aae2c21b8
nJviR <zubanove001@gmail.com>
2024-11-16 21:57:02 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from Generator.Generator import Generator
 
 def main() -> int:
-    len_num = int(input("Введите число от 0 до 50: "))
+    len_num = int(input("Enter a number from 0 to 50: "))
     if len_num <= 0 or len_num >= 50:
-        print("Попробуйте ввести другое число")
+        print("Try entering a different number")
     else:
         gener = Generator(len_num)
         print(f"{gener.createpass()}")
